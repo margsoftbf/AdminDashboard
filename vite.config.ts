5e9f6c31a0a474501b28ade4aac7a742194cef51
Marcin <mgarski@tutanota.com>
2023-12-03 15:30:13 -0500

    Some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
-import ViteCompressionPlugin from 'vite-plugin-compression';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './AdminDashboard/',
-	plugins: [react(), ViteCompressionPlugin()],
+	plugins: [
+		react(),
+		compression({
+      algorithm: 'gzip',
+      filter: (fileName) => /\.(js|css|html|json)$/.test(fileName), 
+      threshold: 1024,
+		}),
+	],
 	optimizeDeps: {
 		esbuildOptions: {
 			plugins: [esbuildCommonjs(['react-calendar', 'react-date-picker'])],
