7aee911af8ea4a45e837307b9e2e04f39b203b39
Marcin <mgarski@tutanota.com>
2023-12-03 15:17:58 -0500

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
+import ViteCompressionPlugin from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './AdminDashboard/',
-  plugins: [react()],
-  optimizeDeps:{
-    esbuildOptions:{
-      plugins:[
-        esbuildCommonjs(['react-calendar','react-date-picker'])
-      ]
-    }
-  }
-})
+	base: './AdminDashboard/',
+	plugins: [react(), ViteCompressionPlugin()],
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [esbuildCommonjs(['react-calendar', 'react-date-picker'])],
+		},
+	},
+});
